69615e0bf9b4d0c57e1721f9332e5a940ead2e5a
rciesielski3 <47296221+rciesielski3@users.noreply.github.com>
2026-07-27 15:04:26 +0200

    chore: update jest config to include qa-news tests (#52)
    Co-authored-by: GitHub Actions <github-actions@github.com>

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  roots: ['<rootDir>/tests'],
+  roots: ['<rootDir>/tests', '<rootDir>/qa-news/src'],
   testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   collectCoverageFrom: [
